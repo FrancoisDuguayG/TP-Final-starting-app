64dbc26980abef4d5f3df72dc4ea3c8ea26de876
FrancoisDuguayG <francois.duguay.g@gmail.com>
2023-05-11 13:35:18 -0400

    Add empty path as exemple

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import "./styles/index.css";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Empty from "./components/Empty";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+  },
+  {
+    path: "/empty",
+    element: <Empty></Empty>,
   },
 ]);
 
